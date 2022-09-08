cfa1be1a1464377fe19285ca01e77341d0304089
Ludmilahttps <ludmila.https@gmail.com>
2022-09-08 10:25:14 -0300

    menu

--- main.cpp
+++ main.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+using namespace std;
 
-Int main()
+int main()
 {
+    int varwhile = 1, menu;
+
+    while (varwhile)
+    {
+        int menu;
+
+        cout << "-----------UFxC String Store V.0--------------" << endl
+             << "1. Insert string" << endl
+             << "2. Print index and string" << endl
+             << "3. Search string (literal)" << endl
+             << "4. Search substrings" << endl
+             << "5. Remove string (by index)" << endl
+             << "6. Remove by substrings (all occurrences)" << endl
+             << "0. Quit" << endl
+             << "----------------------------------------------";
+        cin >> menu;
+
+        switch (menu)
+        {
+        case 1:
+            cout << "1. Insert string" << endl;
+            break;
+
+        default:
+            break;
+        }
+    }
 
     return 0;
 }
