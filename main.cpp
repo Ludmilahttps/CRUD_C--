056e36aff0face404af08168fdcf5502099d2134
Ludmilahttps <ludmila.https@gmail.com>
2022-09-17 22:23:12 -0300

    strings ok

--- main.cpp
+++ main.cpp
@@ -1,56 +1,170 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
+
+int menu();
+int print(vector<string> &list);
+int search_string(vector<string> &list);
+int search_substring(vector<string> &list);
+int remove_string(vector<string> &list);
+int remove_substring(vector<string> &list);
 
 int main()
 {
-    int varwhile = 1, menu;
+    int varwhile = 1;
+    vector<string> list;
+    int option;
 
-    while (varwhile)
+    do
     {
-        int menu;
+        option = menu();
 
-        cout << "-----------UFxC String Store V.0--------------" << endl
-             << "1. Insert string" << endl
-             << "2. Print index and string" << endl
-             << "3. Search string (literal)" << endl
-             << "4. Search substrings" << endl
-             << "5. Remove string (by index)" << endl
-             << "6. Remove by substrings (all occurrences)" << endl
-             << "0. Quit" << endl
-             << "----------------------------------------------" << endl;
-        cin >> menu;
-
-        switch (menu)
+        if (option == 1)
         {
-        case 1:
-            cout << "1. Insert string" << endl;
-            break;
-
-        case 2:
+            string aux;
+            cout << "1. Insert string" << endl
+                 << "Enter string:" << endl;
+            cin >> aux;
+            list.push_back(aux);
+            continue;
+        }
+        if (option == 2)
+        {
             cout << "2. Print index and string" << endl;
-            break;
-
-        case 3:
+            print(list);
+            continue;
+        }
+        if (option == 3)
+        {
             cout << "3. Search string (literal)" << endl;
-            break;
-
-        case 4:
+            search_string(list);
+            continue;
+        }
+        if (option == 4)
+        {
             cout << "4. Search substrings" << endl;
-            break;
-
-        case 5:
+            search_substring(list);
+            continue;
+        }
+        if (option == 5)
+        {
             cout << "5. Remove string (by index)" << endl;
-            break;
-
-        case 6:
+            remove_string(list);
+            continue;
+        }
+        if (option == 6)
+        {
             cout << "6. Remove by substrings (all occurrences)" << endl;
+            remove_substring(list);
             break;
-
-        default:
+        }
+        if (option == 0)
+        {
             cout << "Quit" << endl;
             varwhile = 0;
         }
+    } while (option != 0);
+    return 0;
+}
+
+int menu()
+{
+    int opc;
+    cout << "-----------UFxC String Store V.0--------------" << endl
+         << "1. Insert string" << endl
+         << "2. Print index and string" << endl
+         << "3. Search string (literal)" << endl
+         << "4. Search substrings" << endl
+         << "5. Remove string (by index)" << endl
+         << "6. Remove by substrings (all occurrences)" << endl
+         << "0. Quit" << endl
+         << "----------------------------------------------" << endl;
+    cin >> opc;
+
+    return opc;
+}
+
+int print(vector<string> &list)
+{
+    cout << "list:" << endl;
+    for (size_t i = 0; i < list.size(); i++)
+    {
+        cout << "string " << i << " - " << list.at(i) << endl;
+    }
+}
+
+int search_string(vector<string> &list)
+{
+    string search;
+    int find = 0;
+
+    cout << "Enter string to search" << endl;
+    cin >> search;
+
+    for (size_t i = 0; i < list.size(); i++)
+    {
+        if (list.at(i) == search)
+        {
+            find++;
+            cout << "string " << search << " is found in position " << i << endl;
+        }
+    }
+    if (find == 0)
+    {
+        cout << "string not found" << endl;
+    }
+}
+
+int search_substring(vector<string> &list)
+{
+    string subsearch;
+    int find = 0;
+
+    cout << "Enter substring to search" << endl;
+    cin >> subsearch;
+
+    for (size_t i = 0; i < list.size(); i++)
+    {
+        if (list.at(i) == subsearch)
+        {
+            find++;
+            cout << subsearch << endl;
+        }
+    }
+    if (find == 0)
+    {
+        cout << "string not found" << endl;
+    }
+}
+
+int remove_string(vector<string> &list)
+{
+    int remove;
+    bool find = false;
+
+    cout << "Enter index string to remove" << endl;
+    cin >> remove;
+
+    for (size_t i = 0; i < list.size(); i++)
+    {
+        if (list.at(i) == list.at(remove))
+        {
+            list.erase(list.begin() + i);
+            find = true;
+        }
     }
 
-    return 0;
+    if (find == false)
+    {
+        cout << "String not found" << endl;
+    }
+}
+
+int remove_substring(vector<string> &list)
+{
+    string subremove;
+
+    cout << "Enter substring to remove" << endl;
+    cin >> subremove;
 }
